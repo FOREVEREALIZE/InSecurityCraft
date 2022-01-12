d30c898574cee140df9aa1e306a0b0d366d6978c
felixgames097 <felixcrat@gmail.com>
2022-01-12 16:30:34 +0100

    Do Stuff

---
+++ src/main/java/me/foreverealize/insecuritycraft/ChestCrackerGUI.java
@@ -0,0 +1,10 @@
+package me.foreverealize.insecuritycraft;
+
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class ChestCrackerGUI extends Screen {
+    protected ChestCrackerGUI(Component p_96550_) {
+        super(p_96550_);
+    }
+}
---
+++ src/main/java/me/foreverealize/insecuritycraft/InSecurityCraft.java
@@ -0,0 +1,9 @@
+package me.foreverealize.insecuritycraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("insecuritycraft")
+public class InSecurityCraft {
+    public InSecurityCraft() {}
+}
